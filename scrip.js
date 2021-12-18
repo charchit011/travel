5cd56d9d70ea4e0e68466e7ef14366045bcd5d46
charchit011 <charchitnirayanwal@gmail.com>
2021-12-18 14:35:24 +0530

    add

---
+++ scrip.js
@@ -0,0 +1,7 @@
+
+    var travel=document.getElementById('logo').addEventListener("click",changename);
+
+    function changename(){
+        document.getElementById('logo').innerHTML='haha';
+    }
+    
